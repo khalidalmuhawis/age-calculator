771f6c567b9c4f4b99996fd04a45e922343cebe4
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-08-17 14:11:01 +0300

    age calculator (our second calculator)

--- age_calculator.py
+++ age_calculator.py
@@ -1,14 +1,26 @@
 from datetime import datetime
 
 def check_birthdate(year, month, day):
-	# write code here
+    date1 = datetime(year, month, day)
+    date2 = datetime.now()
+    return date2 > date1
 
 def calculate_age(year, month, day):
-    # write code here
+    birthday = datetime(year, month, day)
+    age = datetime.now() - birthday
+    print("Age: %s"%(age.days/365))
 
 def main():
-	# write main code here
+    year = int(input("Year: "))
+    month = int(input("Month: "))
+    day = int(input("Day: "))
+
+    if check_birthdate(year, month, day):
+        calculate_age(year, month, day)
+    else:
+        print("Invalid Birthdate")
+
 
 
 if __name__ == '__main__':
-	main()
+    main()
